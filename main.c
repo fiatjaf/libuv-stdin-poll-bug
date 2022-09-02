62959a8b96f22c62569853961e84e6d7c32a79a2
fiatjaf <fiatjaf@gmail.com>
2022-09-02 06:58:30 -0300

    experiment with a hacky workaround with epoll directly (failed).

--- main.c
+++ main.c
@@ -1,13 +1,33 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <uv.h>
+#include <sys/epoll.h>
 
-void cb(uv_poll_t *handle, int status, int events) {
+void readstuff() {
   char *line = NULL;
   size_t len = 0;
   getline(&line, &len, stdin);
   fprintf(stdout, "GOT LINE: `%s`\n", line);
   fflush(stdout);
+}
+
+void cb(uv_poll_t *handle, int status, int x) {
+  readstuff();
+
+  int epfd = epoll_create1(0);
+  struct epoll_event event;
+  event.events = EPOLLIN;
+  if (-1 == epoll_ctl(epfd, EPOLL_CTL_ADD, 0, &event)) {
+    exit(3);
+  }
+
+  struct epoll_event* events = malloc(sizeof event);
+  int triggers = epoll_wait(epfd, events, 1, 1000);
+
+  fprintf(stdout, "TRIGGERS: %d\n", triggers);
+  fflush(stdout);
+
+  // readstuff();
 }
 
 int main() {
